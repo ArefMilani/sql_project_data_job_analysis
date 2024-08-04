9a5a507845c6a88e8d3694b177eed74b19c012b9
Aref Milani <arefmilani507@gmail.com>
2024-08-04 04:02:13 +0300

    creating first query

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,16 @@
+/*
+Questions: What are the top-paying data analyst jobs?
+- Identify the top 10 highest-paying Data Analyst roles that are available remotely.
+- Focus on job postings with specified salaries (remove nulls).
+- Why? Highlight the top-paying opportunities for Data Analysts, offering insights into employment options and location flexibility.
+*/
+
+SELECT
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM
+    job_postings_fact;
